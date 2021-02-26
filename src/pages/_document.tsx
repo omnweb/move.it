7aadb2482e50fdd5347aac6b086accb291542da1
netomatiazi <netoomn@gmail.com>
2021-02-26 16:28:46 -0300

    Update metatag og:image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta property="og:title" content="Move.it" />
+                    <meta property="og:image" content={'img-og.jpg'} />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
                     <link rel="icon" href="favicon.png" />
